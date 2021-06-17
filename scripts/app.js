6fb5df859d3d80eb34fe638c737b0711324a1d87
eleftheros <eleftheros_all@protonmail.com>
2021-06-17 13:33:26 +0300

    Update createGrid function

--- scripts/app.js
+++ scripts/app.js
@@ -10,11 +10,23 @@ const createGrid = () => {
   const gridElement = document.createElement("div");
   gridElement.classList.add("poker__container__grid");
   let output = "";
+  let cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 
   for (let i = 0; i < 13; i++) {
     for (let j = 0; j < 13; j++) {
-      output = "" + i + j;
+      output = cards[i] + cards[j];
+
+      if (i < j)
+        output += "s";
+      else if (i > j) {
+        let helper = output[0];
+        output[0] = output[1];
+        output[1] = helper;
+        output += "o";
+      }
+
       const buttonElement = document.createElement("button");
+      buttonElement.classList.add("poker__container__grid__btn");
       buttonElement.textContent = output;
 
       gridElement.appendChild(buttonElement);
